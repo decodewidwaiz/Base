75b13a2f8d778b9c8fdbd06fd9c84299f41ab2d9
Waiz Alam <medecodewidwaiz@gmail.com>
2025-10-01 16:41:00 +0530

    base project setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const port = process.env.BACKEND_PORT || 3000;
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// main route for server's health check
+app.get('/', (req, res) => {
+  res.send('server is running....');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ frontend/Readme.md
@@ -0,0 +1,1 @@
+this is a frontend directory for base of the project. it'll be updated soon.
